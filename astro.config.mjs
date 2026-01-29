4afcd91f57d2af507d231d4f993223b908dcb360
yynmukhtarsyafaat-afk <yynmukhtarsyafaat@gmail.com>
2026-01-29 19:16:41 +0700

    build: update Astro base path to root in Astro configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  base: '/spsmb-musya/',
+  base: '/',
   integrations: [react()],
 
   vite: {
